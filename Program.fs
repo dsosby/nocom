6f8826e2e63aca0267b39b3af77f8590d7f80501
David Sosby <david@sosby.org>
2018-11-21 15:59:39 -0800

    Login and scrape for data cells

--- Program.fs
+++ Program.fs
@@ -1,8 +1,23 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open System
+open FSharp.Data;
 
-open System
+let loginUrl          = "http://192.168.100.1/goform/login"
+let connectionPageUrl = "http://192.168.100.1/MotoConnection.asp"
+let loginUsername     = "admin"
+let loginPassword     = "motorola"
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+    printfn "Logging in"
+    try
+        Http.RequestString(loginUrl, body = FormValues ["loginUsername", loginUsername;
+                                                        "loginPassword", loginPassword]) |> ignore
+        let connectionPage = HtmlDocument.Load(connectionPageUrl)
+        let dataElems =
+            connectionPage.Descendants ["td"]
+            |> Seq.filter(fun cell -> cell.HasClass("moto-content-value") || cell.HasClass("moto-param-title"))
+            |> Seq.toArray
+        printfn "%A" dataElems
+    with
+        | :? System.Net.WebException -> printfn "Modem ded"
+    0
