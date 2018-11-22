eb17b974c6c6fde1a6196e128dab783c6fc5bf24
David Sosby <david@sosby.org>
2018-11-21 18:13:02 -0800

    Commit

--- Program.fs
+++ Program.fs
@@ -8,12 +8,46 @@ let loginPassword     = "motorola"
 
 type CellType = ValueCell | TitleCell | HeaderCell
 
+type DownstreamChannelStatus =
+  {
+    Channel: string;
+    ChannelId: string;
+    LockStatus: string;
+    PowerdBmV: string;
+    SNRdB: string;
+  }
+
+type UpstreamChannelStatus =
+  {
+    Channel: string;
+    ChannelId: string;
+    FreqMhz: string;
+    PowerdBmV: string;
+  }
+
+type ConnectionStatus =
+  {
+    Online: bool;
+    Uptime: string;
+    Downstream: DownstreamChannelStatus[];
+    Upstream: UpstreamChannelStatus[];
+  }
+
+/// From StackOverflow - Seq.split implementation
+let splitBy f input =
+    let i = ref 0
+    input
+    |> Seq.groupBy (fun x ->
+        if f x then incr i
+        !i)
+    |> Seq.map snd
+
 /// Determines cell type of given node
 let getCellType (cell: HtmlNode) =
     match cell with
     | x when x.HasClass("moto-content-value") -> Some ValueCell
     | x when x.HasClass("moto-param-title") -> Some TitleCell
-    | x when x.HasClass("moto-param-header") -> Some HeaderCell
+    // | x when x.HasClass("moto-param-header-s") -> Some HeaderCell // Ignoring these for now
     | _ -> None
 
 /// Fetch tuple types of status tables
@@ -21,8 +55,13 @@ let scrapeStatus (connectionPage: HtmlDocument) =
     connectionPage.Descendants ["td"]
     |> Seq.map (fun cell -> (getCellType cell, cell))
     |> Seq.filter (fun (cellType, _) -> cellType.IsSome)
-    |> Seq.map (fun (cellType, cell) -> (cellType.Value, cell.InnerText()))
-    |> Seq.toArray
+    |> Seq.map (fun (cellType, cell) -> (cellType.Value, cell.InnerText().Trim()))
+    |> splitBy (fun x -> ((=) TitleCell (fst x)))
+
+let structureStatus (statusElements: seq<seq<CellType * string>>) =
+    let status: ConnectionStatus
+    statusElements
+    |> Seq.fold status (fun )
 
 [<EntryPoint>]
 let main argv =
@@ -32,7 +71,7 @@ let main argv =
                                                         "loginPassword", loginPassword]) |> ignore
         let connectionPage = HtmlDocument.Load(connectionPageUrl)
         let statusElements = scrapeStatus connectionPage
-        printfn "%A" statusElements
+        printfn "%A" (Seq.toList statusElements)
     with
         | :? System.Net.WebException -> printfn "Modem ded"
     0
